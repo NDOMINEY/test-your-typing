59f6592379451a94548532b1b3352d9b13649f13
NDOMINEY <natalie.r.dominey@gmail.com>
2023-01-02 21:06:23 +0000

    Change button links to embedded JS due to error found

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,1 @@
-/**
- * button event listeners to direct to different HTML Page
- */
 
-document.getElementById("continue-btn").addEventListener('click', function(){
-    document.location.href = "gameselect.html";
-})
-
-/**
- * 
- */
