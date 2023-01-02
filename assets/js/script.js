57ec25023d92bc49b496840d157c3c1e2827676e
NDOMINEY <natalie.r.dominey@gmail.com>
2023-01-02 19:50:03 +0000

    Add JS to index html button

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * button event listeners to direct to different HTML Page
+ */
+
+document.getElementById("continue-btn").addEventListener('click', function(){
+    document.location.href = "gameselect.html";
+})
+
+/**
+ * 
+ */
