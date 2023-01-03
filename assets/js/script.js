d7491a9ecae88645b5f9b568085cbd67ef5c3d9f
NDOMINEY <natalie.r.dominey@gmail.com>
2023-01-03 18:11:24 +0000

    Add function for random key generator and link to html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,26 @@
+// array for keys to be included in game
 let key = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
 "p","q","r","s","t","u","v","x","w","y","z","0","1","2","3","4","5","6",
-"7","8","9","Backspace","Tab","Enter","Shift","Delete","Space Bar"];
+"7","8","9","Tab","Enter","Shift","Delete","Space Bar"];
 
+//run the game on screen load
+window.onload = runTimedGame();
+
+/**
+ * Runs game cycle
+ */
 function runTimedGame(){
-
+    document.getElementsByClassName("randon-letter")[1].innerHTML = randomKey();
 }
 
+/**
+ * Generates random string from the key array
+ */
 function randomKey(){
-    
+    //generate random number for key array index
+    let i = Math.floor(Math.random() * 42);
+    //returns random index from the key array
+    return key[i];
 }
 
 function checkAnswer(){
