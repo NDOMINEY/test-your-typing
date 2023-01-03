156772dd7b86db4404bce2dd40ee309f3658ca35
NDOMINEY <natalie.r.dominey@gmail.com>
2023-01-03 17:53:05 +0000

    Add array for keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
+let key = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o",
+"p","q","r","s","t","u","v","x","w","y","z","0","1","2","3","4","5","6",
+"7","8","9","Backspace","Tab","Enter","Shift","Delete","Space Bar"];
+
 function runTimedGame(){
 
 }
 
-function randomLetter(){
+function randomKey(){
     
 }
 
